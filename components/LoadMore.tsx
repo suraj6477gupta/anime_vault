3a3e5414854a41abe4a7b7412e65a69f269b9726
suraj6477gupta <surajgupta6477@gmail.com>
2024-08-12 02:58:25 +0530

    Second and Final Commit!!

--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -3,22 +3,32 @@ import { fetchAnime } from "@/app/action";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import { useInView } from "react-intersection-observer";
-import { AnimeProp } from "./AnimeCard";
+import AnimeCard from "./AnimeCard";
+
+let page = 2;
+
+export type AnimeCard = JSX.Element;
 function LoadMore() {
   const {ref, inView}= useInView();
-  const [data, setData]= useState<AnimeProp[]>([]);
+  const [data, setData]= useState<AnimeCard[]>([]);
+  
+
   useEffect(() => {
     if(inView){
       // alert("Load more");
-      fetchAnime(2)
+      fetchAnime(page)
         .then((res) =>{
           setData([...data, ...res])
+          page++;
         })
     }
   }, [inView, data]);
   
   return (
     <>
+      <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
+        {data}
+      </section>
       <section className="flex justify-center items-center w-full">
         <div ref = {ref}>
           <Image
